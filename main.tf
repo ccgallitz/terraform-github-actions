0e50a23f6a404d4c523c7217d5357f0ce020ad6f
Oliver <oliver@ccgallitz.de>
2023-10-13 15:05:39 +0200

    switching network name to variable

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "azurerm_resource_group" "rg-aks" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "myTFVnet2"
+  name                = var.network_name
   address_space       = ["10.0.0.0/16"]
   location            = "westeurope"
   resource_group_name = var.resource_group_name
