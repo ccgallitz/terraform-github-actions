e64a0e4dc6049c9725eef77be223f00702557548
Oliver <oliver@ccgallitz.de>
2023-10-13 14:15:52 +0200

    apply tf fmt manually

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 terraform {
   required_providers {
-    azurerm={
-        source = "hashicorp/azurerm"
-        version=">=3.7.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.7.0"
     }
   }
 
   backend "azurerm" {
-    resource_group_name = "rg-terraform-github-actions-state"
+    resource_group_name  = "rg-terraform-github-actions-state"
     storage_account_name = "tfghactions2305"
-    container_name = "tfstate"
-    key = "terraform.tfstate"
-    use_oidc=true
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
   }
 }
 
@@ -21,6 +21,6 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg-aks" {
-  name = var.resource_group_name
-  location= var.location
+  name     = var.resource_group_name
+  location = var.location
 }
